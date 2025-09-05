cf9071f16ced9dc90987d99eef89517c82e29d8f
Silvarney <silvarney@gmail.com>
2025-09-05 09:32:56 -0300

    Configurando o ALLOWED_HOSTS

--- saas_project/settings.py
+++ saas_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-e#4rszncp$nx#^f@=oceq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
